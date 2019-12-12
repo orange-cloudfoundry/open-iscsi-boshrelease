cc3508daa621d9b9e7db51771b113a87eae1d4d2
obeyler <olivier.beyler@orange.com>
2019-12-12 10:26:31 +0000

    initial

---
+++ download.sh
@@ -0,0 +1,2 @@
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/open-iscsi/open-iscsi_2.0.873+git0.3b4b4500-14ubuntu3.7_amd64.deb
+
---
+++ jobs/open-iscsi-pkg-install/templates/bin/pre-start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+DEBS_PACKAGES=/var/vcap/packages/open-iscsi
+
+install_package() {
+  local package=$1
+
+  count_failure=0
+  until dpkg -i "$package"; do
+    if [[ $count_failure -ge 100 ]]; then
+      echo "Failed installing package $package after 100 attempts."
+      exit 1
+    fi
+    echo "Retrying '$package'"
+    count_failure=$(($count_failure+1))
+    sleep 2
+  done
+}
+
+install_package "${DEBS_PACKAGES:?}/open-iscsi_2.0.873+git0.3b4b4500-14ubuntu3.7_amd64.deb"
+sudo sed -i 's/node.session.timeo.replacement_timeout = 120/node.session.timeo.replacement_timeout = 400/g' /etc/iscsi/iscsid.conf
+service open-iscsi restart
+
+echo "done prestart, open-iscsi service is configured"
+cat /etc/iscsi/initiatorname.iscsi
+sudo systemctl status iscsid
+sudo mount --make-rshared /
