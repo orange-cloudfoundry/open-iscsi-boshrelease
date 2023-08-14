489deca1ee9c9a001028276c06ecbf559f45365a
Pierre Oblin <pierre.oblin@orange.com>
2023-08-14 14:22:04 +0000

    bump to open-iscsi_2.1.8-1ubuntu1_amd64.deb

--- vendor.sh
+++ vendor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 vendir sync -f vendir.yml --lock-file vendir.lock
-bosh add-blob src/debs/open-iscsi/open-iscsi_2.0.873+git0.3b4b4500-14ubuntu3.7_amd64.deb open-iscsi/open-iscsi.deb
+bosh add-blob src/debs/open-iscsi/open-iscsi_2.1.8-1ubuntu1_amd64.deb open-iscsi/open-iscsi.deb
 
