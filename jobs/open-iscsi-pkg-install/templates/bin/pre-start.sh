82093861f21c5ce4394c07d0029d25b5894afc22
Pierre Oblin <pierre.oblin@orange.com>
2023-08-14 17:18:29 +0000

    uploaded blobs

--- jobs/open-iscsi-pkg-install/templates/bin/pre-start.sh
+++ jobs/open-iscsi-pkg-install/templates/bin/pre-start.sh
@@ -8,7 +8,7 @@ install_package() {
   local package=$1
 
   count_failure=0
-  until dpkg -i "$package"; do
+  until dpkg --force-confnew -i "$package"; do
     if [[ $count_failure -ge 100 ]]; then
       echo "Failed installing package $package after 100 attempts."
       exit 1
@@ -21,7 +21,7 @@ install_package() {
 
 
 install_package "${DEBS_PACKAGES:?}/libopeniscsiusr.deb"
-install_package "${DEBS_PACKAGES:?}/libisns.deb"
+install_package "${DEBS_PACKAGES:?}/libisns0.deb"
 install_package "${DEBS_PACKAGES:?}/open-iscsi.deb"
 sudo sed -i 's/node.session.timeo.replacement_timeout = 120/node.session.timeo.replacement_timeout = 400/g' /etc/iscsi/iscsid.conf
 service open-iscsi restart
